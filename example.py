023d0ea93cee609415162966138d12f9c61f256c
Yanko <kurata.yuca@gmail.com>
2025-06-16 05:47:59 +0900

    first commit

---
+++ example.py
@@ -0,0 +1,17 @@
+print("hello world")
+
+def greet():
+    return "こんにちは"
+print(greet())
+
+def print_name(name):
+    print(f"私の名前は{name}です")
+print_name("taro")
+
+def get_greet():
+    return("おはようございます")
+print(get_greet())
+
+def add(a, b):
+    return(a + b)
+print(add(1, 2))
